3a2f7a0f833512f22b56c6748865a3cfa9ac4987
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-14 12:35:21 -0800

    Done.

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,25 @@
 import React from "react"
 
-const PizzaForm = () => {
+const PizzaForm = (props) => {
+
+  const handleEditForm = (event) => {
+    let editObj = {
+      [event.target.name]: event.target.value
+    }
+    return props.onEditForm(editObj)
+  }
+
+  const { topping, size } = props.editingPizza
+  const vegetarian = props.editingPizza.vegetarian
   return(
-      <div className="form-row">
+      <div className="form-row" onChange={handleEditForm}>
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
+            <input type="text" className="form-control" name="topping" placeholder="Pizza Topping" value={
+                topping
               }/>
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select value={size} className="form-control" name="size">
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,13 +27,13 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+            <input  className="form-check-input" type="radio" value="Vegetarian" name='vegetarian' checked={vegetarian ? true : false} />
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input className="form-check-input" type="radio" value="Not Vegetarian" name='vegetarian' checked={vegetarian ? false : true}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import Pizza from '../components/Pizza'
 class PizzaList extends Component {
 
+  handlePizzaRender = () => {
+    return this.props.pizzas.map(pizza => {
+      return <Pizza pizza={pizza} key={pizza.id} onEditPizza={this.props.onEditPizza}/>
+    })
+  }
+
   render() {
     return (
       <table className="table table-striped">
@@ -15,7 +21,7 @@ class PizzaList extends Component {
         </thead>
         <tbody>
           {
-            //render Pizza here
+            this.handlePizzaRender()
           }
         </tbody>
       </table>
